a0d6a28baea7d72c017134867bd25098fd4a44b4
Alexandra Usacheva <lazercore@me.com>
2018-08-22 17:18:33 +0300

    FE-646: PR fixes

--- src/app/papi/params/payout-search-params.ts
+++ src/app/papi/params/payout-search-params.ts
@@ -4,7 +4,7 @@ export class PayoutSearchParams {
     public fromTime?: string;
     public toTime?: string;
     public status?: PayoutStatus;
-    public payoutIds?: string[] | string;
+    public payoutIds?: string[];
     public fromId?: string;
     public size?: string;
 }
