47012dca6ababb74306220b807a426959955a316
Alexandra Usacheva <lazercore@me.com>
2018-09-12 19:40:23 +0300

    FE-663: clean up

--- src/app/claim/claim.module.ts
+++ src/app/claim/claim.module.ts
@@ -31,22 +31,12 @@ import { ClaimInfoDetailsComponent } from './claim-info/claim-info-details/claim
 import { PartyModificationsComponent } from './party-modifications/party-modifications.component';
 import { PartyModificationContainerComponent } from './party-modification-container/party-modification-container.component';
 import { UnitActionsComponent } from './unit-actions/unit-actions.component';
-import { CreateLegalAgreementComponent } from './create-change/create-legal-agreement/create-legal-agreement.component';
-import { CreateCategoryRefComponent } from './create-change/create-category-ref/create-category-ref.component';
 import { ClaimService } from './claim.service';
-import { CreateCurrencyRefComponent } from './create-change/create-currency-ref/create-currency-ref.component';
-import { CreateContractTemplateComponent } from './create-change/create-contract-template/create-contract-template.component';
 import { AcceptClaimComponent } from './accept-claim/accept-claim.component';
 import { DenyClaimComponent } from './deny-claim/deny-claim.component';
-import { CreateBusinessScheduleRefComponent } from './create-change/create-business-schedule-ref/create-business-schedule-ref.component';
-import {
-    CreateServiceAcceptanceActPreferencesComponent
-} from './create-change/create-service-acceptance-act-preferences/create-service-acceptance-act-preferences.component';
 import { CreateTerminalObjectComponent } from './create-change/create-terminal-object/create-terminal-object.component';
 import { SharedModule } from '../shared/shared.module';
 import { ContainerNamePipe } from './container-name.pipe';
-import { CreateLocationComponent } from './create-change/create-location/create-location.component';
-import { CreateDetailsComponent } from './create-change/create-details/create-details.component';
 import { PartyModificationUnitsComponent } from './party-modification-units/party-modification-units.component';
 import { PartyModificationCreationModule } from '../party-modification-creation';
 import { CreateModificationComponent } from './create-modification/create-modification.component';
@@ -88,14 +78,6 @@ import { CreateModificationComponent } from './create-modification/create-modifi
         PartyModificationsComponent,
         PartyModificationContainerComponent,
         UnitActionsComponent,
-        CreateLegalAgreementComponent,
-        CreateCategoryRefComponent,
-        CreateCurrencyRefComponent,
-        CreateContractTemplateComponent,
-        CreateBusinessScheduleRefComponent,
-        CreateServiceAcceptanceActPreferencesComponent,
-        CreateLocationComponent,
-        CreateDetailsComponent,
         AcceptClaimComponent,
         DenyClaimComponent,
         CreateTerminalObjectComponent,
