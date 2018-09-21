da1c6085369c5307f40be475bc6e0a59feca8979
Alexandra Usacheva <lazercore@me.com>
2018-09-21 16:55:36 +0300

    FE-663: pr fix

--- src/app/party-modification-creation/party-modification-creation.component.ts
+++ src/app/party-modification-creation/party-modification-creation.component.ts
@@ -9,14 +9,14 @@ import {
 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import isObject from 'lodash-es/isObject';
-import mapValues from 'lodash-es/mapValues';
+import assign from 'lodash-es/assign';
+import transform from 'lodash-es/transform';
+import { isDate } from 'moment';
 
 import { PartyModification } from '../damsel/payment-processing';
 import { ContractModificationName, ShopModificationName } from '../claim/model';
 import { toPartyModification } from './to-party-modification';
 import { ActionType, ModificationAction } from '../claim/modification-action';
-import { assign, isEmpty, pickBy, reduce, transform } from 'lodash-es';
-import { isDate } from 'moment';
 
 @Component({
     selector: 'cc-party-modification-creation',
