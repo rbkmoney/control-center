318d4633976a67a14dea9410d15327fa2b6422a6
Alexandra Usacheva <lazercore@me.com>
2018-09-26 19:16:10 +0300

    FE-668: PR fast fix

--- src/app/party-modification-creation/contract/report-preferences/report-preferences.component.ts
+++ src/app/party-modification-creation/contract/report-preferences/report-preferences.component.ts
@@ -1,23 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'cc-report-preferences',
     templateUrl: 'report-preferences.component.html'
 })
-export class ReportPreferencesComponent implements OnInit {
+export class ReportPreferencesComponent {
 
     @Input()
     form: FormGroup;
 
     constructor(private fb: FormBuilder) {
-    }
-
-    ngOnInit() {
-        this.form.registerControl('serviceAcceptanceActPreferences', this.fb.group({
-            schedule: this.fb.group({}),
-            signer: this.fb.group({})
-        }));
     }
 
     toggleCheckbox(e, data) {
