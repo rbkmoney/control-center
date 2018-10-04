e29148eb87c72868890f7ecd79b76c8f7bf85d4b
Rinat Arsaev <krickray@gmail.com>
2018-10-04 11:58:09 +0300

    refactor

--- src/app/payment-adjustment/payment-adjustment.service.ts
+++ src/app/payment-adjustment/payment-adjustment.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { of, Subject } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
 
 import { ReportService } from '../papi/report.service';
 import { SearchFormParams } from './search-form/search-form-params';
 import { MerchantStatisticsService } from '../thrift/merchant-statistics.service';
-import { StatPayment } from '../gen-damsel/merch_stat';
+import { StatPayment, StatResponse } from '../gen-damsel/merch_stat';
 import { QueryDSL } from '../magista';
 
 @Injectable()
@@ -16,13 +16,26 @@ export class PaymentAdjustmentService {
     constructor(private reportService: ReportService, private merchantStatisticsService: MerchantStatisticsService) {
     }
 
-    getFilteredPayments(payments: StatPayment[], invoicesIds?: string[]) {
+    fetchPayments(params: SearchFormParams): void {
+        const {invoicesIds} = params;
+        this.payments$.next(null);
+        this.getAllPayments(params).subscribe(
+            (response) => this.payments$.next(this.getFilteredPayments(response, invoicesIds)),
+            (e) => this.clearPayments()
+        );
+    }
+
+    clearPayments(): void {
+        this.payments$.next([]);
+    }
+
+    private getFilteredPayments(payments: StatPayment[], invoicesIds?: string[]): StatPayment[] {
         return invoicesIds && invoicesIds.length
             ? payments.filter((payment) => invoicesIds.find((id) => id === payment.invoiceId))
             : payments;
     }
 
-    getPayments(params: SearchFormParams, continuationToken?: string) {
+    private getPayments(params: SearchFormParams, continuationToken?: string): Observable<StatResponse> {
         const {fromRevision, partyId, fromTime, toTime} = params;
         return this.merchantStatisticsService.getPayments({
             dsl: JSON.stringify({
@@ -39,23 +52,10 @@ export class PaymentAdjustmentService {
         });
     }
 
-    getAllPayments(params: SearchFormParams, continuationToken?: string, payments: StatPayment[] = []) {
+    private getAllPayments(params: SearchFormParams, continuationToken?: string, payments: StatPayment[] = []): Observable<StatPayment[]> {
         return this.getPayments(params, continuationToken).pipe(mergeMap((response) => {
             const mergedPayments = [...payments, ...response.data.payments];
             return response.continuationToken ? this.getAllPayments(params, response.continuationToken) : of(mergedPayments);
         }));
-    }
-
-    fetchPayments(params: SearchFormParams): void {
-        const {invoicesIds} = params;
-        this.payments$.next(null);
-        this.getAllPayments(params).subscribe(
-            (response) => this.payments$.next(this.getFilteredPayments(response, invoicesIds)),
-            (e) => this.clearPayments()
-        );
-    }
-
-    clearPayments() {
-        this.payments$.next([]);
     }
 }
