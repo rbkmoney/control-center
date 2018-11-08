2a74aacdf4c561f24c20a8cb162cd051b0c6c421
Rinat Arsaev <krickray@gmail.com>
2018-11-08 12:59:07 +0300

    fix typedef

--- src/app/metadata/metadata.service.ts
+++ src/app/metadata/metadata.service.ts
@@ -131,6 +131,9 @@ export class MetadataService {
                     switch (structureName) {
                         case 'typedef':
                             result = new TypeDef();
+                            toCookList.push(() => {
+                                result.type = get(item.type, name);
+                            });
                             break;
                         case 'const':
                             result = new Const();
