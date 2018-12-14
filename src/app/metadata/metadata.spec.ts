1f3e4d5e4aa2efb0b96e1af9c5646aa9049fa639
Rinat Arsaev <krickray@gmail.com>
2018-12-14 17:13:05 +0300

    check not equal

--- src/app/metadata/metadata.spec.ts
+++ src/app/metadata/metadata.spec.ts
@@ -1,20 +1,29 @@
 import { MetadataService, Type } from './metadata.service';
 import domainTestData from './domain-test-data.json';
+import domainTestDataChanged from './domain-test-data-changed.json';
 
 describe('MetadataService', () => {
     let metadataService: MetadataService;
     let metadata: Type;
     let initValue: any;
+    let initValueEqual: any;
+    let initValueChanged: any;
 
     beforeEach(() => {
         metadataService = new MetadataService();
         metadata = metadataService.get('Domain', 'domain');
         initValue = metadata.toThrift(domainTestData);
+        initValueEqual = metadata.toThrift(domainTestData);
+        initValueChanged = metadata.toThrift(domainTestDataChanged);
     });
 
     describe('isEqualValue', () => {
         it('equal', async () => {
-            expect(MetadataService.isEqualValue(initValue, initValue)).toBeTruthy();
+            expect(MetadataService.isEqualValue(initValue, initValueEqual)).toBeTruthy();
+        });
+
+        it('is not equal', async () => {
+            expect(MetadataService.isEqualValue(initValue, initValueChanged)).toBeFalsy();
         });
     });
 });
