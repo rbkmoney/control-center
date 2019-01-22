50e4347050100d7d8ef357e5d2983cc70f8de3bd
DenisEzhov <d.ezhov@rbkmoney.com>
2019-01-22 16:19:15 +0300

    unused functions removed

--- src/app/party-modification-creation/contract/payout-tool-info/wallet-info/wallet-info.component.ts
+++ src/app/party-modification-creation/contract/payout-tool-info/wallet-info/wallet-info.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { MatCheckboxChange } from '@angular/material';
 import * as uuid from 'uuid';
 
 @Component({
@@ -12,29 +11,11 @@ export class WalletInfoComponent implements OnInit {
     @Input()
     form: FormGroup;
 
-    isBankDetails = false;
-
-    isCorrespondentAccount = false;
-
     constructor(private fb: FormBuilder) {
     }
 
     ngOnInit() {
         this.form.registerControl('walletID', this.fb.control(''));
-    }
-
-    detailsChange(change: MatCheckboxChange) {
-        this.isBankDetails = change.checked;
-        change.checked
-            ? this.form.registerControl('bank', this.fb.group({}))
-            : this.form.removeControl('bank');
-    }
-
-    accountChange(change: MatCheckboxChange) {
-        this.isCorrespondentAccount = change.checked;
-        change.checked
-            ? this.form.registerControl('correspondentAccount', this.fb.group({}))
-            : this.form.removeControl('correspondentAccount');
     }
 
     generate() {
