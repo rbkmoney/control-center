cf99ec4f38a200dc22c2402d6279e863c867152a
DenisEzhov <d.ezhov@rbkmoney.com>
2019-02-18 19:13:17 +0300

    walletID edit fix

--- src/app/party-modification-creation/contract/payout-tool-info/wallet-info/wallet-info.component.ts
+++ src/app/party-modification-creation/contract/payout-tool-info/wallet-info/wallet-info.component.ts
@@ -12,6 +12,6 @@ export class WalletInfoComponent implements OnInit {
     constructor(private fb: FormBuilder) {}
 
     ngOnInit() {
-        this.form.registerControl('walletID', this.fb.control(''));
+        this.form.registerControl('walletId', this.fb.control(''));
     }
 }
