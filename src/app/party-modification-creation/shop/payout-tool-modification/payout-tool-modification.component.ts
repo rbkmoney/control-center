103b2b02477569314e9b28a7260119f0ea878fc9
Denis Ezhov <ezhoden@mail.ru>
2019-02-26 15:05:47 +0300

    FE-764. Редактирование несохраненных modifications (#54)

--- src/app/party-modification-creation/shop/payout-tool-modification/payout-tool-modification.component.ts
+++ src/app/party-modification-creation/shop/payout-tool-modification/payout-tool-modification.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { get } from 'lodash-es';
+
+import { PayoutToolID } from '../../../gen-damsel/domain';
 
 @Component({
     selector: 'cc-shop-payout-tool-modification',
@@ -9,9 +12,14 @@ export class PayoutToolModificationComponent implements OnInit {
     @Input()
     form: FormGroup;
 
+    @Input()
+    initialValue: PayoutToolID;
+
     constructor(private fb: FormBuilder) {}
 
     ngOnInit() {
-        this.form.setControl('modification', this.fb.control('', Validators.required));
+        const payoutToolId = get(this, 'initialValue.modification', '');
+        this.form.setControl('modification', this.fb.control(payoutToolId, Validators.required));
+        this.form.updateValueAndValidity();
     }
 }
--- src/app/party-modification-creation/shop/shop-params/shop-params.component.ts
+++ src/app/party-modification-creation/shop/shop-params/shop-params.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { get } from 'lodash-es';
+
+import { ShopParams } from '../../../gen-damsel/payment_processing';
 
 @Component({
     selector: 'cc-shop-params',
@@ -9,20 +12,38 @@ export class ShopParamsComponent implements OnInit {
     @Input()
     form: FormGroup;
 
+    @Input()
+    initialValue: ShopParams;
+
+    showCategory = false;
+
     constructor(private fb: FormBuilder) {}
 
     ngOnInit() {
-        this.form.registerControl('contractId', this.fb.control('', Validators.required));
-        this.form.registerControl('payoutToolId', this.fb.control('', Validators.required));
+        const contractId = get(this, 'initialValue.contractId', '');
+        const payoutToolId = get(this, 'initialValue.payoutToolId', '');
+        this.form.registerControl('contractId', this.fb.control(contractId, Validators.required));
+        this.form.registerControl(
+            'payoutToolId',
+            this.fb.control(payoutToolId, Validators.required)
+        );
         this.form.registerControl('details', this.fb.group({}));
         this.form.registerControl('location', this.fb.group({}));
+        this.toggleCategory();
+        this.form.updateValueAndValidity();
     }
 
-    toggleCheckbox(e, data) {
-        if (e.checked) {
-            this.form.registerControl(data, this.fb.group({}));
+    toggleCheckbox(show: boolean, controlName: string, data: object = {}) {
+        if (show) {
+            this.form.registerControl(controlName, this.fb.group(data));
         } else {
-            this.form.removeControl(data);
+            this.form.removeControl(controlName);
         }
+    }
+
+    toggleCategory() {
+        const category = get(this, 'initialValue.category', null);
+        this.showCategory = category !== null;
+        this.toggleCheckbox(this.showCategory, 'category', category);
     }
 }
