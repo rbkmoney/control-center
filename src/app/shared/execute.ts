f971e5e24e063bf21fe7d6d5d8f3f56dcdce21c2
Rinat Arsaev <krickray@gmail.com>
2019-03-04 14:49:39 +0300

    add execute result ids

--- src/app/shared/execute.ts
+++ src/app/shared/execute.ts
@@ -4,6 +4,7 @@ type Fn = () => Observable<any>;
 
 interface BaseResult {
     func: Fn;
+    idx: number;
 }
 
 interface ErrorResult extends BaseResult {
@@ -16,14 +17,15 @@ interface SuccessResult extends BaseResult {
 
 type Result = ErrorResult | SuccessResult;
 
-async function exec(funcs: Fn[], result$: Subject<Result>): Promise<void> {
+async function exec(funcs: Fn[], srcFuncs: Fn[], result$: Subject<Result>): Promise<void> {
     let func: Fn;
     while ((func = funcs.pop())) {
+        const idx = srcFuncs.findIndex(f => f === func);
         try {
             const data = await func().toPromise();
-            result$.next({ func, data });
+            result$.next({ func, idx, data });
         } catch (error) {
-            result$.next({ func, error });
+            result$.next({ func, idx, error });
         }
     }
 }
@@ -33,7 +35,7 @@ export function execute(funcs: Fn[], execCount = 4): Observable<Result> {
     const tmpFuncs = funcs.slice();
     const execs: Promise<void>[] = [];
     for (let i = 0; i < execCount; ++i) {
-        execs.push(exec(tmpFuncs, result$));
+        execs.push(exec(tmpFuncs, funcs, result$));
     }
     Promise.all(execs)
         .then(() => result$.complete())
