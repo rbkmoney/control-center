11b23f0ad730988a483dab7806d44bf927af65e7
Ildar Galeev <keinasylum@gmail.com>
2019-04-17 17:20:44 +0300

    Added monaco diff editor support

---
+++ src/app/monaco-editor/abstarct-monaco-directive.ts
@@ -0,0 +1,76 @@
+import {
+    OnInit,
+    Input,
+    ElementRef,
+    Output,
+    EventEmitter,
+    Directive,
+    OnChanges,
+    SimpleChanges,
+    HostListener,
+    OnDestroy
+} from '@angular/core';
+
+import {
+    MonacoFile,
+    IEditorOptions,
+    CodeLensProvider,
+    IDisposable,
+    CompletionProvider
+} from './model';
+import { AbstractMonacoService } from './abstract-monaco.service';
+
+@Directive({})
+export abstract class AbstractMonacoDirective implements OnInit, OnChanges, OnDestroy {
+    @Input() options: IEditorOptions;
+    @Input() codeLensProviders: CodeLensProvider[];
+    @Input() completionProviders: CompletionProvider[];
+
+    @Output() fileChange = new EventEmitter<MonacoFile>();
+    @Output() ready = new EventEmitter();
+    @Output() codeLensProviderRegistered = new EventEmitter<IDisposable[]>();
+    @Output() completionProviderRegistered = new EventEmitter<IDisposable[]>();
+
+    @HostListener('window:resize') onResize() {
+        this.monacoEditorService.resize();
+    }
+
+    constructor(
+        protected monacoEditorService: AbstractMonacoService,
+        protected editorRef: ElementRef
+    ) {}
+
+    ngOnChanges(changes: SimpleChanges) {
+        this.childOnChanges(changes);
+        if (changes.options) {
+            this.monacoEditorService.updateOptions(changes.options.currentValue);
+        }
+        if (changes.codeLensProviders) {
+            this.monacoEditorService.addCodeLensProvider(changes.codeLensProviders.currentValue);
+        }
+        if (changes.completionProviders) {
+            this.monacoEditorService.addCompletionProvider(
+                changes.completionProviders.currentValue
+            );
+        }
+    }
+
+    ngOnInit() {
+        this.monacoEditorService
+            .init(this.editorRef, this.options)
+            .subscribe(() => this.ready.emit());
+        this.monacoEditorService.fileChange.subscribe(file => this.fileChange.emit(file));
+        this.monacoEditorService
+            .codeLensProviderRegistered()
+            .subscribe(disposible => this.codeLensProviderRegistered.emit(disposible));
+        this.monacoEditorService
+            .completionProviderRegistered()
+            .subscribe(disposible => this.completionProviderRegistered.emit(disposible));
+    }
+
+    ngOnDestroy() {
+        this.monacoEditorService.destroy();
+    }
+
+    abstract childOnChanges(changes: SimpleChanges);
+}
--- src/app/monaco-editor/monaco-editor.module.ts
+++ src/app/monaco-editor/monaco-editor.module.ts
@@ -5,11 +5,13 @@ import { MonacoEditorDirective } from './monaco-editor.directive';
 import { MonacoEditorService } from './monaco-editor.service';
 import { CodeLensService } from './providers/code-lens.service';
 import { CompletionService } from './providers/completion.service';
+import { MonacoDiffEditorDirective } from './monaco-diff-editor.directive';
+import { MonacoDiffEditorService } from './monaco-diff-editor.service';
 
 @NgModule({
-    declarations: [MonacoEditorDirective],
+    declarations: [MonacoEditorDirective, MonacoDiffEditorDirective],
     imports: [CommonModule],
-    exports: [MonacoEditorDirective],
-    providers: [MonacoEditorService, CodeLensService, CompletionService]
+    exports: [MonacoEditorDirective, MonacoDiffEditorDirective],
+    providers: [MonacoEditorService, CodeLensService, CompletionService, MonacoDiffEditorService]
 })
 export class MonacoEditorModule {}
