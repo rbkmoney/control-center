9d5e7ef1b1a9130344be47a95b35cbe5c9688c14
Aleksandra Usacheva <lazercore@me.com>
2019-12-18 18:00:47 +0300

    clean up

--- src/app/claim-mgt/claim/claim.component.ts
+++ src/app/claim-mgt/claim/claim.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { tap } from 'rxjs/operators';
 
 import { ClaimService } from './claim.service';
 
