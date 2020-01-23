807e05ad3d3ee1869c7f39bc124a5851a3c69251
Aleksandra Usacheva <lazercore@me.com>
2020-01-23 21:46:49 +0300

    build fox

--- src/app/claim-mgt/claim/status-changer/status-changer.service.ts
+++ src/app/claim-mgt/claim/status-changer/status-changer.service.ts
@@ -72,7 +72,7 @@ export class StatusChangerService {
                 filter(v => {
                     return this.formPrevValue ? v.reason === this.formPrevValue.reason : false;
                 }),
-                tap(v => this.formPrevValue = v)
+                tap(v => this.formPrevValue = v as any)
             )
             .subscribe(v => {
                 const { type } = v;
