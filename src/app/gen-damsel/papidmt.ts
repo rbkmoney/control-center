390734af3e32b0f379ac1a29b16a564354cab9df
Rinat Arsaev <krickray@gmail.com>
2020-02-03 12:37:08 +0300

    remove gen-damsel

--- src/app/gen-damsel/papidmt.ts
+++
@@ -1,8 +0,0 @@
-// tslint:disable
-
-import Int64 from "thrift-ts/lib/int64";
-
-import * as domain_config from "./domain_config";
-export interface HistoryWrapper {
-  history: domain_config.History;
-}
--- src/app/gen-damsel/signer.ts
+++
@@ -1,3 +0,0 @@
-// tslint:disable
-
-import Int64 from "thrift-ts/lib/int64";
