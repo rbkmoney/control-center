99cadab0f85f54129fc7fdbf041e300f8fb25bfc
Rinat Arsaev <krickray@gmail.com>
2020-02-05 14:53:48 +0300

    add vsc templates

---
+++ .vsc-templates/README.md
@@ -0,0 +1,6 @@
+# VSC scaffolding
+
+-   [Visual Studio Code extension](https://marketplace.visualstudio.com/items?itemName=alfnielsen.vsc-scaffolding)
+
+-   [Documentation](http://vsc-base.org/)
+-   [Source code](https://github.com/alfnielsen/vsc-base)
---
+++ .vsc-templates/module-with-component.vsc-template.ts
@@ -0,0 +1,117 @@
+import * as vsc from 'vsc-base';
+
+const PREFIX = 'cc';
+
+function getModuletName(name: string) {
+    return `${vsc.toPascalCase(name)}Module`;
+}
+
+function getModulePath(name: string) {
+    return `${vsc.toKebabCase(name)}.module`;
+}
+
+function getComponentName(name: string) {
+    return `${vsc.toPascalCase(name)}Component`;
+}
+
+function getComponentPath(name: string) {
+    return `${vsc.toKebabCase(name)}.component`;
+}
+
+function getCssClassName(name: string) {
+    return `${PREFIX}-${vsc.toKebabCase(name)}`;
+}
+
+export function Template(path: string, templatePath: string): vsc.vscTemplate {
+    return {
+        userInputs: [
+            {
+                title: 'Component name',
+                argumentName: 'name',
+                defaultValue: ''
+            }
+        ],
+        template: [
+            {
+                type: 'folder',
+                name: inputs => vsc.toKebabCase(inputs.name),
+                children: [
+                    {
+                        type: 'file',
+                        name: inputs => `${getModulePath(inputs.name)}.ts`,
+                        content: inputs => `
+import { NgModule } from '@angular/core';
+
+import { ${getComponentName(inputs.name)} } from './${getComponentPath(inputs.name)}';
+
+@NgModule({
+    imports: [],
+    declarations: [${getComponentName(inputs.name)}],
+    exports: [${getComponentName(inputs.name)}]
+})
+export class ${getModuletName(inputs.name)} {}
+`
+                    },
+                    {
+                        type: 'file',
+                        name: inputs => `${getComponentPath(inputs.name)}.html`,
+                        content: inputs => `
+<div class="${PREFIX}-${vsc.toKebabCase(inputs.name)}">
+    <ng-content></ng-content>
+</div>
+`
+                    },
+                    {
+                        type: 'file',
+                        name: inputs => `${getComponentPath(inputs.name)}.scss`,
+                        content: inputs => `
+.${getCssClassName(inputs.name)} {
+}
+`
+                    },
+                    {
+                        type: 'file',
+                        name: inputs => `${getComponentPath(inputs.name)}.ts`,
+                        content: inputs => `
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+    selector: '${PREFIX}-${vsc.toKebabCase(inputs.name)}',
+    templateUrl: '${getComponentPath(inputs.name)}.html',
+    styleUrls: ['${getComponentPath(inputs.name)}.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ${getComponentName(inputs.name)} {
+}
+`
+                    },
+                    {
+                        type: 'file',
+                        name: inputs => `index.ts`,
+                        content: inputs => `
+export * from './${getModulePath(inputs.name)}';
+export * from './${getComponentPath(inputs.name)}'
+`
+                    },
+                    {
+                        type: 'file',
+                        name: inputs => `_${vsc.toKebabCase(inputs.name)}-theme.scss`,
+                        content: inputs => `
+@import '~@angular/material/theming';
+
+@mixin ${getCssClassName(inputs.name)}-theme($theme) {
+    .${getCssClassName(inputs.name)} {
+    }
+}
+
+@mixin ${getCssClassName(inputs.name)}-typography($config) {
+    .${getCssClassName(inputs.name)} {
+    }
+}
+`
+                    }
+                ]
+            }
+        ]
+    };
+}
