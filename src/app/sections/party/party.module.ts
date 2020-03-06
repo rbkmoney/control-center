9d0f2d4345af3ff9ff3ac508f33b694f5635baca
Aleksandra Usacheva <lazercore@me.com>
2020-03-06 19:05:55 +0300

    LGTM fix

--- src/app/sections/party/party.module.ts
+++ src/app/sections/party/party.module.ts
@@ -3,8 +3,6 @@ import { PartyMgtRouting } from './party-routing.module';
 import { PartyComponent } from './party.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { CommonModule } from '@angular/common';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatCommonModule } from '@angular/material/core';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
