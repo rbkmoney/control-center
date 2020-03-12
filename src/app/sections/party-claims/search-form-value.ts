f5204919109aeb6ddc4a242642fb9d8d701dcd76
Aleksandra Usacheva <lazercore@me.com>
2020-03-12 16:34:01 +0300

    clean up

--- src/app/sections/party-claims/search-form-value.ts
+++ src/app/sections/party-claims/search-form-value.ts
@@ -1,9 +1,7 @@
-import * as domain from '../../thrift-services/damsel/gen-model/domain';
 import { ClaimID } from '../../thrift-services/damsel/gen-model/claim_management';
 import { ClaimStatus } from '../../papi/model';
 
 export interface SearchFormValue {
-    party_id?: domain.PartyID;
     claim_id?: ClaimID
     statuses?: ClaimStatus[];
 }
