b53b9bbd055367529eb5e487695b01f46f10768a
Aleksandra Usacheva <lazercore@me.com>
2020-03-20 03:45:31 +0300

    imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrettyJsonModule } from 'angular2-prettyjson';
+import { FlexModule } from '@angular/flex-layout';
 
 import { CurrencyPipe, FormatAmountPipe, ThriftViewPipe, ClaimStatusPipe } from './pipes';
 import { ThriftInt64Pipe } from './thrift-int64.pipe';
 import { PrettyJsonComponent } from './components/pretty-json/pretty-json.component';
 import { CardContainerComponent } from './components/card-container/card-container.component';
 import { SearchComponentEmptyComponent } from './components/search-component-empty/search-component-empty.component';
-import { FlexModule } from '@angular/flex-layout';
 
 const declarations = [
     CurrencyPipe,
