a6beda1ffadcc85391589186306bee130d90a809
Артем <WWW_cool@inbox.ru>
2020-03-24 16:49:58 +0300

    FF-1017: Claims search form (#127)
    * added search form
    
    * fixed
    
    * fixed
    
    * fixed

--- src/app/sections/search-claims/search-claims.component.ts
+++ src/app/sections/search-claims/search-claims.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SearchFormValue } from './search-form/search-form-value';
+
 @Component({
     templateUrl: './search-claims.component.html',
     styleUrls: ['./search-claims.component.scss']
@@ -8,4 +10,8 @@ export class SearchClaimsComponent implements OnInit {
     constructor() {}
 
     ngOnInit() {}
+
+    search(searchFormValue: SearchFormValue) {
+        console.log(searchFormValue);
+    }
 }
--- src/app/sections/search-claims/search-claims.module.ts
+++ src/app/sections/search-claims/search-claims.module.ts
@@ -1,11 +1,47 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatTableModule
+} from '@angular/material';
 
 import { SearchClaimsComponent } from './search-claims.component';
 import { SearchClaimsComponentRouting } from './search-claims-routing.module';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-    declarations: [SearchClaimsComponent],
-    imports: [CommonModule, SearchClaimsComponentRouting]
+    imports: [
+        CommonModule,
+        SearchClaimsComponentRouting,
+        MatButtonModule,
+        MatCardModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatMenuModule,
+        MatProgressBarModule,
+        MatSelectModule,
+        MatSnackBarModule,
+        MatTableModule,
+        ReactiveFormsModule,
+        FlexLayoutModule,
+        SharedModule,
+        MatExpansionModule
+    ],
+    declarations: [SearchClaimsComponent, SearchFormComponent]
 })
 export class SearchClaimsModule {}
