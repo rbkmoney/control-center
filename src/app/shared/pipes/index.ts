cbc8faa55e43ff01892d26132068746c091bfc38
Aleksandra Usacheva <lazercore@me.com>
2020-03-24 16:17:03 +0300

    source pipe added

--- src/app/shared/pipes/index.ts
+++ src/app/shared/pipes/index.ts
@@ -2,3 +2,4 @@ export * from './claim-status.pipe';
 export * from './currency.pipe';
 export * from './format-amount.pipe';
 export * from './thrift-view.pipe';
+export * from './claim-source.pipe';
