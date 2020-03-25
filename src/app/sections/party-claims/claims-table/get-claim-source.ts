f10334db48a36ccd75ae5fb53b420c5101179428
Aleksandra Usacheva <lazercore@me.com>
2020-03-25 12:31:01 +0300

    clean up

--- src/app/sections/party-claims/claims-table/get-claim-source.ts
+++
@@ -1,4 +0,0 @@
-import { Claim } from '../../../thrift-services/damsel/gen-model/claim_management';
-
-export const getClaimSource = (claim: Claim): string =>
-    claim.changeset[0].user_info.type.external_user ? 'Dashboard' : 'Control Center';
