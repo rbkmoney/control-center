09b0dc82e6a3e7dc8fe6657ee2de7acf145812a3
Aleksandra Usacheva <lazercore@me.com>
2020-03-25 13:20:54 +0300

    string to claimstatus pipe

---
+++ src/app/sections/party-claims/search-form/claim-status-from-string.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { ClaimStatus } from '../../../papi/model';
+
+@Pipe({
+    name: 'ccClaimStatusFromString'
+})
+export class ClaimStatusFromStringPipe implements PipeTransform {
+    transform(s: string): string {
+        switch (s) {
+            case ClaimStatus.review:
+                return 'Review';
+            case ClaimStatus.revoked:
+                return 'Revoked';
+            case ClaimStatus.denied:
+                return 'Denied';
+            case ClaimStatus.pending_acceptance:
+                return 'Pending Acceptance';
+            case ClaimStatus.pending:
+                return 'Pending';
+            case ClaimStatus.accepted:
+                return 'Accepted';
+            default:
+                return s;
+        }
+    }
+}
