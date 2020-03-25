8b1c5ba4bec1ae796e2f769e91a3c013f20b0ffe
Aleksandra Usacheva <lazercore@me.com>
2020-03-25 04:23:10 +0300

    linter

--- src/app/sections/party-claims/search-form/to-form-value.ts
+++ src/app/sections/party-claims/search-form/to-form-value.ts
@@ -3,11 +3,13 @@ import { Params } from '@angular/router';
 export const toFormValue = (q: Params) => {
     const result = {};
     for (const k in q) {
-        if (k === 'statuses' && typeof q[k] === 'string') {
-            result[k] = [q[k]];
-            break;
+        if (q.hasOwnProperty(k)) {
+            if (k === 'statuses' && typeof q[k] === 'string') {
+                result[k] = [q[k]];
+                break;
+            }
+            result[k] = q[k];
         }
-        result[k] = q[k];
     }
     return result;
 };
--- src/app/shared/operators/remove-empty-properties.ts
+++ src/app/shared/operators/remove-empty-properties.ts
@@ -1,13 +1,11 @@
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { isNil } from 'lodash-es';
 
 export const removeEmptyProperties = <T>(s: Observable<T>) =>
     s.pipe(
         map(obj =>
             Object.keys(obj).reduce(
-                (acc, cur) =>
-                    !isNil(obj[cur]) && obj[cur] !== '' ? { ...acc, [cur]: obj[cur] } : acc,
+                (acc, cur) => (!!obj[cur] && obj[cur] !== '' ? { ...acc, [cur]: obj[cur] } : acc),
                 {} as T
             )
         )
