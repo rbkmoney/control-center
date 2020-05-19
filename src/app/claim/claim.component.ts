dc4ba694db8a40798ec85931fd7a28f3f86330a5
Aleksandra Usacheva <lazercore@me.com>
2020-05-19 19:12:22 +0300

    Create claim fix (#136)
    * Create claim fix

--- src/app/claim/claim.component.ts
+++ src/app/claim/claim.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 import { ClaimService } from './claim.service';
 
@@ -14,14 +15,18 @@ export class ClaimComponent {
         private claimService: ClaimService,
         private snackBar: MatSnackBar
     ) {
-        this.route.params.subscribe((params) => {
-            const { action, party_id, claim_id } = params;
-            this.claimService.resolveClaimInfo(action, party_id, claim_id).subscribe({
-                error: (error) => {
+        this.route.params
+            .pipe(
+                switchMap(({ action, party_id, claim_id }) =>
+                    this.claimService.resolveClaimInfo(action, party_id, claim_id)
+                )
+            )
+            .subscribe(
+                () => {},
+                (error) => {
                     console.error(error);
                     this.snackBar.open('An error occurred while claim resolving', 'OK');
-                },
-            });
-        });
+                }
+            );
     }
 }
