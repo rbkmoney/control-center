3f4936156bb8becb732798bc48622f3f987e673c
WWW_cool <WWW_cool@inbox.ru>
2020-05-21 11:48:19 +0300

    added search claims

---
+++ src/app/sections/search-claims/search-claims.module.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+
+import { SharedModule } from '../../shared/shared.module';
+import { ClaimManagementService } from '../../thrift-services/damsel/claim-management.service';
+import { ClaimSearchFormModule } from '../claim-search-form';
+import { SearchClaimsComponentRouting } from './search-claims-routing.module';
+import { SearchClaimsComponent } from './search-claims.component';
+import { SearchClaimsService } from './search-claims.service';
+import { ClaimMailPipePipe } from './search-table/claim-mail-pipe.pipe';
+import { SearchTableComponent } from './search-table/search-table.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        SearchClaimsComponentRouting,
+        MatButtonModule,
+        MatCardModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatMenuModule,
+        MatProgressBarModule,
+        MatSelectModule,
+        MatSnackBarModule,
+        MatTableModule,
+        ReactiveFormsModule,
+        FlexLayoutModule,
+        SharedModule,
+        MatExpansionModule,
+        ClaimSearchFormModule,
+    ],
+    declarations: [SearchClaimsComponent, SearchTableComponent, ClaimMailPipePipe],
+    providers: [SearchClaimsService, ClaimManagementService],
+})
+export class SearchClaimsModule {}
---
+++ src/app/sections/search-claims/search-table/search-table.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Claim } from '../../../thrift-services/damsel/gen-model/claim_management';
+
+@Component({
+    selector: 'cc-search-table',
+    templateUrl: './search-table.component.html',
+    styleUrls: ['./search-table.component.scss'],
+})
+export class SearchTableComponent {
+    @Input()
+    claims: Claim[];
+
+    displayedColumns = ['claimID', 'party', 'source', 'status', 'updatedAt', 'actions'];
+
+    constructor(private router: Router) {}
+
+    navigateToClaim(partyID: string, claimID: number) {
+        this.router.navigate(['claim-mgt', 'party', partyID, 'claim', claimID + '']);
+    }
+}
