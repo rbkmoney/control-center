2ad1462cae5c6378697bc940654a38a1dc4fd805
Ildar Galeev <KeinAsylum@gmail.com>
2020-05-22 14:52:01 +0300

    Added email search claims param (#138)

--- src/app/sections/claim-search-form/search-form-value.ts
+++ src/app/sections/claim-search-form/search-form-value.ts
@@ -3,6 +3,7 @@ import { ClaimID } from '../../thrift-services/damsel/gen-model/claim_management
 
 export interface SearchFormValue {
     claim_id?: ClaimID;
+    email?: string;
     party_id?: string;
     statuses?: ClaimStatus[];
 }
