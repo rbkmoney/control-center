59a4580696fbcc2dcd161bb8091160efe933df9c
Rinat Arsaev <krickray@gmail.com>
2020-06-16 02:16:59 +0500

    support slashes for ids

--- src/app/repairing/add-ids-input/add-ids-input.component.ts
+++ src/app/repairing/add-ids-input/add-ids-input.component.ts
@@ -4,8 +4,6 @@ import { FormControl } from '@angular/forms';
 @Component({
     selector: 'cc-add-ids-input',
     templateUrl: 'add-ids-input.component.html',
-    styleUrls: [],
-    providers: [],
 })
 export class AddIdsInputComponent {
     @Input()
@@ -24,7 +22,7 @@ export class AddIdsInputComponent {
 
     execIdsFromStr(str: string) {
         const ids: string[] = [];
-        const selectIds = /[a-z0-9-]+/gi;
+        const selectIds = /[a-z0-9-\/]+/gi;
         let execId = selectIds.exec(str);
         while (execId) {
             const id = execId[0];
