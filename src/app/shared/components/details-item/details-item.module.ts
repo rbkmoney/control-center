de875d743e4d88c97cd3f7abbacdab6ef0d91f08
Aleksandra Usacheva <lazercore@me.com>
2020-07-07 12:30:07 +0300

    FE-1062: questionary redesign

--- src/app/shared/components/details-item/details-item.module.ts
+++ src/app/shared/components/details-item/details-item.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FlexModule } from '@angular/flex-layout';
 
 import { DetailsItemComponent } from './details-item.component';
 
 @NgModule({
     declarations: [DetailsItemComponent],
     exports: [DetailsItemComponent],
+    imports: [FlexModule],
 })
 export class DetailsItemModule {}
