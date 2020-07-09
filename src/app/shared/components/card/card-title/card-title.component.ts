743681faa9e12c906db6b3afaba0fbf3d32d9444
Aleksandra Usacheva <lazercore@me.com>
2020-07-09 15:20:10 +0300

    FE-1062: clean up

--- src/app/shared/components/card/card-title/card-title.component.ts
+++ src/app/shared/components/card/card-title/card-title.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'cc-card-title',
     templateUrl: 'card-title.component.html',
-    styleUrls: ['card-title.component.scss'],
 })
 export class CardTitleComponent {}
