ef24649fa82c5e76288a81615208c6ed205132c7
Aleksandra Usacheva <lazercore@me.com>
2020-08-14 16:38:14 +0300

    FE-1062: мутации

--- src/app/shared/components/pretty-json/json-clean-look.pipe.ts
+++ src/app/shared/components/pretty-json/json-clean-look.pipe.ts
@@ -1,31 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import isArray from 'lodash-es/isArray';
-import cloneDeep from 'lodash-es/cloneDeep';
-import transform from 'lodash-es/transform';
+
+import { skipNullValues } from '../../utils';
 
 @Pipe({
     name: 'ccJsonCleanLook',
 })
 export class JsonCleanLookPipe implements PipeTransform {
     transform(obj: object, isActive: boolean): object {
-        return isActive ? this.clean(cloneDeep(obj)) : obj;
-    }
-
-    private clean(obj) {
-        return transform(
-            obj,
-            (acc, v, k) => {
-                if (v !== null) {
-                    if (isArray(v)) {
-                        acc[k] = v.map((o) => this.clean(o));
-                    } else if (typeof v === 'object') {
-                        acc[k] = this.clean(v);
-                    } else {
-                        acc[k] = v;
-                    }
-                }
-            },
-            {}
-        );
+        return isActive ? skipNullValues(obj) : obj;
     }
 }
