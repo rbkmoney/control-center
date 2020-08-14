f481e90e9b42c2f74269a901004beb9370dbbef8
Aleksandra Usacheva <lazercore@me.com>
2020-08-14 16:45:28 +0300

    FE-1062: мутации

--- src/app/shared/utils/skip-null-values.ts
+++ src/app/shared/utils/skip-null-values.ts
@@ -1,8 +1,8 @@
 import isArray from 'lodash-es/isArray';
 import transform from 'lodash-es/transform';
 
-export const skipNullValues = (obj: object): object => {
-    return transform(
+export const skipNullValues = (obj: object): object =>
+    transform(
         obj,
         (acc, v, k) => {
             if (v === null) {
@@ -19,4 +19,3 @@ export const skipNullValues = (obj: object): object => {
         },
         {}
     );
-};
