81e90dde5753f4473516fccd3cfcb1cd77e2a7c4
Rinat Arsaev <krickray@gmail.com>
2020-09-01 12:48:23 +0300

    extract services

--- src/app/sections/payment-routing-rules/shop-payment-routing-ruleset/add-shop-payment-routing-rule-dialog/add-shop-payment-routing-rule-dialog.component.ts
+++ src/app/sections/payment-routing-rules/shop-payment-routing-ruleset/add-shop-payment-routing-rule-dialog/add-shop-payment-routing-rule-dialog.component.ts
@@ -1,122 +1,50 @@
 import { Component, Inject } from '@angular/core';
-import { FormArray, FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { of } from 'rxjs';
-import { startWith, switchMap } from 'rxjs/operators';
 
-import { DomainTypedManager, PaymentRoutingRulesService } from '../../../../thrift-services';
+import { DomainTypedManager } from '../../../../thrift-services';
 import { Predicate, RiskScore } from '../../../../thrift-services/damsel/gen-model/domain';
-
-enum TerminalType {
-    new = 'new',
-    existent = 'existent',
-}
+import {
+    AddShopPaymentRoutingRuleDialogService,
+    TerminalType,
+} from './add-shop-payment-routing-rule-dialog.service';
 
 @Component({
     selector: 'cc-add-shop-payment-routing-rule-dialog',
     templateUrl: 'add-shop-payment-routing-rule-dialog.component.html',
     styleUrls: ['add-shop-payment-routing-rule-dialog.component.scss'],
+    providers: [AddShopPaymentRoutingRuleDialogService],
 })
 export class AddShopPaymentRoutingRuleDialogComponent {
-    form = this.fb.group({
-        description: '',
-        weight: '',
-        priority: 1000,
-        terminalType: [null, Validators.required],
-        existentTerminalID: ['', Validators.required],
-        newTerminal: this.fb.group({
-            name: ['', Validators.required],
-            description: ['', Validators.required],
-            riskCoverage: [null, Validators.required],
-            options: this.fb.array([this.createOption()]),
-        }),
-    });
+    form = this.addShopPaymentRoutingRuleDialogService.form;
+    newTerminalOptionsForm = this.addShopPaymentRoutingRuleDialogService.newTerminalOptionsForm;
+
     terminalType = TerminalType;
     riskScore = RiskScore;
     terminals$ = this.domainTypedManager.getTerminalObjects();
-    predicateValid: boolean;
-    predicate: Predicate;
 
-    get newTerminalOptionsForm() {
-        return this.form.get('newTerminal').get('options') as FormArray;
-    }
+    predicate: Predicate;
+    predicateValid: boolean;
 
     constructor(
-        private fb: FormBuilder,
+        private addShopPaymentRoutingRuleDialogService: AddShopPaymentRoutingRuleDialogService,
         private dialogRef: MatDialogRef<AddShopPaymentRoutingRuleDialogComponent>,
-        private paymentRoutingRulesService: PaymentRoutingRulesService,
         private domainTypedManager: DomainTypedManager,
         @Inject(MAT_DIALOG_DATA) public data: { partyID: string; refID: number }
-    ) {
-        this.form
-            .get('terminalType')
-            .valueChanges.pipe(startWith(this.form.value.terminalType))
-            .subscribe((type) => {
-                const { newTerminal, existentTerminalID } = this.form.controls;
-                switch (type) {
-                    case TerminalType.new:
-                        newTerminal.enable();
-                        existentTerminalID.disable();
-                        return;
-                    case TerminalType.existent:
-                        newTerminal.disable();
-                        existentTerminalID.enable();
-                        return;
-                    default:
-                        newTerminal.disable();
-                        existentTerminalID.disable();
-                        return;
-                }
-            });
-    }
+    ) {}
 
     add() {
-        const {
-            description,
-            weight,
-            priority,
-            terminalType,
-            existentTerminalID,
-            newTerminal,
-        } = this.form.value;
-        (terminalType === TerminalType.new
-            ? this.domainTypedManager.createTerminal({
-                  terminalName: newTerminal.name,
-                  terminalDescription: newTerminal.description,
-                  riskCoverage: newTerminal.riskCoverage,
-                  options: newTerminal.options,
-              })
-            : of(existentTerminalID)
-        )
-            .pipe(
-                switchMap((terminalID) =>
-                    this.paymentRoutingRulesService.addShopRule({
-                        description,
-                        weight,
-                        priority,
-                        terminalID,
-                        refID: this.data.refID,
-                        partyID: this.data.partyID,
-                        predicate: this.predicate,
-                    })
-                )
-            )
-            .subscribe(() => this.dialogRef.close());
+        this.addShopPaymentRoutingRuleDialogService.add(this.predicate);
     }
 
     cancel() {
         this.dialogRef.close();
     }
 
-    private createOption() {
-        return this.fb.group({ key: ['', Validators.required], value: ['', Validators.required] });
-    }
-
     addOption() {
-        this.newTerminalOptionsForm.push(this.createOption());
+        this.addShopPaymentRoutingRuleDialogService.addOption();
     }
 
     removeOption(idx: number) {
-        this.newTerminalOptionsForm.removeAt(idx);
+        this.addShopPaymentRoutingRuleDialogService.removeOption(idx);
     }
 }
