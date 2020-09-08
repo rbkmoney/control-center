f2ac1a2df562ab603c164cb3a31a2cfe78be7561
Aleksandra Usacheva <lazercore@me.com>
2020-09-08 12:03:01 +0300

    FE-1062: files module

--- src/app/sections/party-claim/party-claim-actions/file-uploader/file-uploader.component.ts
+++ src/app/sections/party-claim/party-claim-actions/file-uploader/file-uploader.component.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Modification } from '../../../../thrift-services/damsel/gen-model/claim_management';
+import { FileUploaderService } from './file-uploader.service';
 
 @Component({
     selector: 'cc-file-uploader',
     templateUrl: 'file-uploader.component.html',
     styleUrls: ['file-uploader.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [FileUploaderService],
 })
 export class FileUploaderComponent {
     @Output()
@@ -14,4 +16,13 @@ export class FileUploaderComponent {
 
     @Input()
     disabled: boolean;
+
+    startUploading$ = this.fileUploaderService.startUploading$;
+    inProgress$ = this.fileUploaderService.inProgress$;
+
+    constructor(private fileUploaderService: FileUploaderService) {}
+
+    startUploading(files: File[]) {
+        this.startUploading$.next(files);
+    }
 }
