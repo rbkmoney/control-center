a5c6685e5d48ad5419c564ed85e31bba9df638f8
DenisHedgehog <ezhoden@mail.ru>
2020-09-16 19:17:25 +0300

    unused func removed

--- src/app/sections/party-payments/payments-table/payments-table.component.ts
+++ src/app/sections/party-payments/payments-table/payments-table.component.ts
@@ -1,14 +1,6 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { pluck, switchMap } from 'rxjs/operators';
 
-import { PartyService } from '../../../party/party.service';
-import {
-    InvoiceID,
-    InvoicePaymentID,
-    Shop,
-} from '../../../thrift-services/damsel/gen-model/domain';
+import { InvoiceID, InvoicePaymentID } from '../../../thrift-services/damsel/gen-model/domain';
 import { StatPayment } from '../../../thrift-services/damsel/gen-model/merch_stat';
 import { NavigationParams } from '../navigation-params';
 
@@ -30,16 +22,7 @@ export class PaymentsTableComponent {
 
     displayedColumns: string[] = ['amount', 'status', 'createdAt', 'shop', 'actions'];
 
-    constructor(private route: ActivatedRoute, private partyService: PartyService) {}
-
     navigateToPayment(invoiceID: InvoiceID, paymentID: InvoicePaymentID) {
         this.goToPaymentDetails.emit({ invoiceID, paymentID });
-    }
-
-    getShop(id: string): Observable<Shop> {
-        return this.route.params.pipe(
-            pluck('partyID'),
-            switchMap((partyID) => this.partyService.getShop(partyID, id))
-        );
     }
 }
