dc70b997d476ed801956fb6522c48eb3e51d850d
Aleksandra Usacheva <lazercore@me.com>
2020-09-21 17:25:11 +0300

    FE-1062: clean up

--- src/app/party-modifications-extractor/party-modifications-extractor-dialog.service.ts
+++ src/app/party-modifications-extractor/party-modifications-extractor-dialog.service.ts
@@ -8,15 +8,6 @@ import { createContractCreation, createPayoutToolCreation, createShopCreation } 
 import { createContractor } from './creators/create-contractor';
 import { createShopAccountCreation } from './creators/create-shop-account-creation';
 import { ExtractFormValue } from './extract-form-value';
-// import { MatDialog } from '@angular/material/dialog';
-// import { MatSnackBar } from '@angular/material/snack-bar';
-// import { Observable, Subject } from 'rxjs';
-// import { filter, switchMap } from 'rxjs/operators';
-//
-// import { Questionary } from '../thrift-services/ank/gen-model/questionary_manager';
-// import { Modification } from '../thrift-services/damsel/gen-model/claim_management';
-// import { PartyID } from '../thrift-services/damsel/gen-model/domain';
-// import { PartyModificationsExtractorDialogComponent } from './party-modifications-extractor.component';
 
 @Injectable()
 export class PartyModificationsExtractorDialogService {
@@ -72,34 +63,4 @@ export class PartyModificationsExtractorDialogService {
 
         return result;
     }
-    // private extract$ = new Subject<{ partyID: PartyID; questionary: Questionary }>();
-    //
-    // extracted$: Observable<Modification[]> = this.extract$.pipe(
-    //     switchMap(
-    //         ({ partyID, questionary }) =>
-    //             new Observable<Modification[]>((observer) => {
-    //                 const dialog = this.dialog
-    //                     .open(PartyModificationsExtractorDialogComponent, {
-    //                         disableClose: true,
-    //                         data: { questionary, partyID },
-    //                         width: '800px',
-    //                     })
-    //
-    //                     .afterClosed()
-    //                     .pipe(filter((r) => r.length > 0))
-    //                     .subscribe((result) => {
-    //                         this.snackBar.open('Party modifications extracted successfully', 'OK', {
-    //                             duration: 1500,
-    //                         });
-    //                         observer.next(result.map((item) => ({ party_modification: item })));
-    //                     });
-    //             })
-    //     )
-    // );
-    //
-    // constructor(private dialog: MatDialog, private snackBar: MatSnackBar) {}
-    //
-    // extract(partyID: PartyID, questionary: Questionary) {
-    //     this.extract$.next({ partyID, questionary });
-    // }
 }
