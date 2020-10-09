010d141393ba6059f91576742b211be4337997b0
Aleksandra Usacheva <lazercore@me.com>
2020-10-09 03:45:07 +0300

    Payments init.

--- src/app/sections/search-payments/search-payments.component.ts
+++ src/app/sections/search-payments/search-payments.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 
+import { SearchFiltersParams } from '@cc/app/shared/components/payments-search-filters/search-filters-params';
+
 @Component({
     templateUrl: 'search-payments.component.html',
 })
-export class SearchPaymentsComponent {}
+export class SearchPaymentsComponent {
+    searchParamsChanges($event: SearchFiltersParams) {
+        console.log($event);
+    }
+}
--- src/app/sections/search-payments/search-payments.module.ts
+++ src/app/sections/search-payments/search-payments.module.ts
@@ -1,10 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+import {
+    PaymentsMainSearchFiltersModule,
+    PaymentsOtherSearchFiltersModule,
+} from '@cc/app/shared/components';
 
 import { SearchPaymentsRoutingModule } from './search-payments-routing.module';
 import { SearchPaymentsComponent } from './search-payments.component';
 
 @NgModule({
-    imports: [SearchPaymentsRoutingModule],
+    imports: [
+        SearchPaymentsRoutingModule,
+        MatCardModule,
+        PaymentsMainSearchFiltersModule,
+        FlexModule,
+        PaymentsOtherSearchFiltersModule,
+        MatProgressBarModule,
+        CommonModule,
+    ],
     declarations: [SearchPaymentsComponent],
 })
 export class SearchPaymentsModule {}
