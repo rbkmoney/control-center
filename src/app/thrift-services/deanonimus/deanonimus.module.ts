43dadd81000380053d2fc4dda2771e5c1fb218b3
Aleksandra Usacheva <lazercore@me.com>
2020-10-12 17:53:15 +0300

    Merge with master

---
+++ src/app/thrift-services/deanonimus/deanonimus.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+import { DeanonimusService } from './deanonimus.service';
+
+@NgModule({
+    providers: [DeanonimusService],
+})
+export class DeanonimusModule {}
--- src/app/thrift-services/index.ts
+++ src/app/thrift-services/index.ts
@@ -5,3 +5,4 @@ export * from './ank';
 export * from './file-storage';
 export * from './messages';
 export * from './damsel';
+export * from './deanonimus';
