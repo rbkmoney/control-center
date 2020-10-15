3020f102de3d6d970337c8db1368f394524de3d6
Rinat Arsaev <krickray@gmail.com>
2020-10-15 16:20:17 +0300

    query params store to shared

--- src/app/sections/party-chargebacks/chargebacks-search-filters-store.service.ts
+++ src/app/sections/party-chargebacks/chargebacks-search-filters-store.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import pickBy from 'lodash-es/pickBy';
 
-import { removeEmptyProperties, wrapValuesToArray } from '@cc/utils/index';
+import { QueryParamsStore } from '@cc/app/shared/services';
+import { wrapValuesToArray } from '@cc/utils/index';
 
-import { QueryParamsStore } from '../party-payments/query-params-store';
 import { FormValue } from './chargebacks-search-filters';
 
 const ARRAY_PARAMS: (keyof FormValue)[] = [
