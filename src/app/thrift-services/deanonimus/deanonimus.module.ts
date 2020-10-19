c0a3b7a8701e6301d232e46a4fca82855561ab86
Aleksandra Usacheva <lazercore@me.com>
2020-10-19 11:40:21 +0300

    Deanonimus added

---
+++ src/app/thrift-services/deanonimus/deanonimus.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { DeanonimusService } from './deanonimus.service';
+
+@NgModule({
+    providers: [DeanonimusService],
+})
+export class DeanonimusModule {}
