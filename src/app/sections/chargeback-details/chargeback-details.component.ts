ffc4f1a46d6651ab59bf82965d3d271cf2a3add9
Rinat Arsaev <krickray@gmail.com>
2020-10-28 16:40:06 +0300

    Update chargebacks (#206)

--- src/app/sections/chargeback-details/chargeback-details.component.ts
+++ src/app/sections/chargeback-details/chargeback-details.component.ts
@@ -2,11 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { combineLatest } from 'rxjs';
-import { first, switchMap } from 'rxjs/operators';
+import { delay, first, map, pluck, shareReplay, switchMap } from 'rxjs/operators';
 import {
     InvoicePaymentChargebackStage,
     InvoicePaymentChargebackStatus,
 } from 'src/app/thrift-services/damsel/gen-model/domain';
+
+import { getUnionKey } from '@cc/utils/index';
 
 import { ChangeChargebackStatusDialogComponent } from './change-chargeback-status-dialog';
 import { ChargebackDetailsService } from './chargeback-details.service';
@@ -36,6 +38,18 @@ export class ChargebackDetailsComponent {
     shop$ = this.chargebackDetailsService.shop$;
     payment$ = this.chargebackDetailsService.payment$;
 
+    reopenAvailable$ = this.chargeback$.pipe(
+        map((c) => getUnionKey(c.status) === 'rejected' && getUnionKey(c.stage) !== 'arbitration'),
+        shareReplay(1)
+    );
+
+    changeStatusAvailable$ = this.chargeback$.pipe(
+        pluck('status'),
+        map(getUnionKey),
+        map((s) => s === 'pending'),
+        shareReplay(1)
+    );
+
     constructor(
         private chargebackDetailsService: ChargebackDetailsService,
         private dialog: MatDialog,
@@ -57,7 +71,8 @@ export class ChargebackDetailsComponent {
                             },
                         })
                         .afterClosed()
-                )
+                ),
+                delay(1000)
             )
             .subscribe(() => this.chargebackDetailsService.loadChargeback());
     }
@@ -66,18 +81,21 @@ export class ChargebackDetailsComponent {
         combineLatest([this.chargeback$, this.payment$])
             .pipe(
                 first(),
-                switchMap(([{ id: chargebackID }, { id: paymentID, invoice_id: invoiceID }]) =>
-                    this.dialog
-                        .open(ReopenChargebackDialogComponent, {
-                            ...ReopenChargebackDialogComponent.defaultConfig,
-                            data: {
-                                invoiceID,
-                                paymentID,
-                                chargebackID,
-                            },
-                        })
-                        .afterClosed()
-                )
+                switchMap(
+                    ([{ id: chargebackID, stage }, { id: paymentID, invoice_id: invoiceID }]) =>
+                        this.dialog
+                            .open(ReopenChargebackDialogComponent, {
+                                ...ReopenChargebackDialogComponent.defaultConfig,
+                                data: {
+                                    invoiceID,
+                                    paymentID,
+                                    chargebackID,
+                                    stage: getUnionKey(stage),
+                                },
+                            })
+                            .afterClosed()
+                ),
+                delay(1000)
             )
             .subscribe(() => this.chargebackDetailsService.loadChargeback());
     }
