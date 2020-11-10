54ba5ff7edf022e389015637d72c102feb4ac9b8
Aleksandra Usacheva <lazercore@me.com>
2020-11-11 01:29:41 +0300

    Bye party-old route

--- src/app/papi/party.service.ts
+++ src/app/papi/party.service.ts
@@ -6,7 +6,7 @@ import { map } from 'rxjs/operators';
 import { decode } from '@cc/utils/java-thrift-formatter';
 
 import { ConfigService } from '../core/config.service';
-import { Party } from '../thrift-services/damsel/gen-model/domain';
+import { Contract, Party, PayoutTool, Shop } from '../thrift-services/damsel/gen-model/domain';
 import { ContractTemplate } from './model';
 
 @Injectable()
@@ -22,4 +22,23 @@ export class PartyService {
             .get<ContractTemplate[]>(`${this.papiEndpoint}/parties/${partyId}`)
             .pipe(map((party) => decode(party)));
     }
+
+    getShops = (partyID: string): Observable<Shop[]> =>
+        this.getParty(partyID).pipe(map((party) => Array.from(party.shops.values())));
+
+    getShop = (partyID: string, shopID: string): Observable<Shop> =>
+        this.getShops(partyID).pipe(map((shops) => shops.find((shop) => shop.id === shopID)));
+
+    getContracts = (partyID: string): Observable<Contract[]> =>
+        this.getParty(partyID).pipe(map((party) => Array.from(party.contracts.values())));
+
+    getContract = (partyID: string, contractID: string): Observable<Contract> =>
+        this.getContracts(partyID).pipe(
+            map((contracts) => contracts.find((contract) => contract.id === contractID))
+        );
+
+    getPayoutTools = (partyID: string, contractID: string): Observable<PayoutTool[]> =>
+        this.getContract(partyID, contractID).pipe(
+            map((contract) => Array.from(contract.payout_tools.values()))
+        );
 }
--- src/app/sections/payment-details/payment-details.service.ts
+++ src/app/sections/payment-details/payment-details.service.ts
@@ -4,7 +4,7 @@ import { progress } from '@rbkmoney/partial-fetcher/dist/progress';
 import { combineLatest, of } from 'rxjs';
 import { map, pluck, shareReplay, switchMap } from 'rxjs/operators';
 
-import { PartyService } from '../../party/party.service';
+import { PartyService } from '../../papi/party.service';
 import { QueryDSL } from '../../query-dsl';
 import { MerchantStatisticsService } from '../../thrift-services/damsel/merchant-statistics.service';
 
