f34dbd11ce561865b431242e1948280731b60de8
Aleksandra Usacheva <lazercore@me.com>
2020-11-19 03:00:50 +0300

    Clean up

--- src/app/papi/party.service.ts
+++ src/app/papi/party.service.ts
@@ -1,12 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, pluck } from 'rxjs/operators';
+import { map, pluck, shareReplay } from 'rxjs/operators';
 
 import { decode } from '@cc/utils/java-thrift-formatter';
 
 import { ConfigService } from '../core/config.service';
-import { Contract, ContractID, Party, PartyID } from '../thrift-services/damsel/gen-model/domain';
+import {
+    Contract,
+    ContractID,
+    ContractorID,
+    Party,
+    PartyContractor,
+    PartyID,
+} from '../thrift-services/damsel/gen-model/domain';
 
 @Injectable()
 export class PartyService {
@@ -17,13 +24,20 @@ export class PartyService {
     }
 
     getParty = (partyID: PartyID): Observable<Party> =>
-        this.http
-            .get<Party>(`${this.papiEndpoint}/parties/${partyID}`)
-            .pipe(map((party) => decode(party)));
+        this.http.get<Party>(`${this.papiEndpoint}/parties/${partyID}`).pipe(
+            map((party) => decode(party)),
+            shareReplay(1)
+        );
 
     getContracts = (partyID: PartyID): Observable<Map<ContractID, Contract>> =>
         this.getParty(partyID).pipe(pluck('contracts'));
 
     getContract = (partyID: PartyID, contractID: ContractID): Observable<Contract> =>
         this.getContracts(partyID).pipe(map((contracts) => contracts.get(contractID)));
+
+    getContractors = (partyID: PartyID): Observable<Map<ContractorID, PartyContractor>> =>
+        this.getParty(partyID).pipe(pluck('contractors'));
+
+    getContractor = (partyID: PartyID, contractorID: ContractorID): Observable<PartyContractor> =>
+        this.getContractors(partyID).pipe(map((contractors) => contractors.get(contractorID)));
 }
--- src/app/sections/payment-details/payment-main-info/contractor-details/fetch-contractor.service.ts
+++ src/app/sections/payment-details/payment-main-info/contractor-details/fetch-contractor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { combineLatest, Subject } from 'rxjs';
-import { map, pluck, shareReplay, switchMap } from 'rxjs/operators';
+import { forkJoin, of, Subject } from 'rxjs';
+import { pluck, shareReplay, switchMap } from 'rxjs/operators';
 
 import { PartyService } from '../../../../papi/party.service';
 import { ContractID, PartyID } from '../../../../thrift-services/damsel/gen-model/domain';
@@ -11,12 +11,11 @@ export class FetchContractorService {
 
     contractor$ = this.getContractor$.pipe(
         switchMap(({ partyID, contractID }) =>
-            combineLatest([
-                this.partyService.getParty(partyID),
-                this.partyService.getContract(partyID, contractID),
-            ])
+            forkJoin([of(partyID), this.partyService.getContract(partyID, contractID)])
         ),
-        map(([party, contract]) => party.contractors.get(contract.contractor_id)),
+        switchMap(([partyID, { contractor_id }]) =>
+            this.partyService.getContractor(partyID, contractor_id)
+        ),
         pluck('contractor'),
         shareReplay(1)
     );
