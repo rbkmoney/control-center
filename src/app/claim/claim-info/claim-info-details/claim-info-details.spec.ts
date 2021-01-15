0b43bf3c5d3dc999774ba919ec6fc6fad81deeb0
Ildar Galeev <keinasylum@gmail.com>
2021-01-15 16:10:25 +0300

    Linter fix

--- src/app/claim/claim-info/claim-info-details/claim-info-details.spec.ts
+++ src/app/claim/claim-info/claim-info-details/claim-info-details.spec.ts
@@ -3,21 +3,29 @@ import { TestBed, waitForAsync } from '@angular/core/testing';
 import { ClaimInfoDetailsComponent } from './claim-info-details.component';
 
 describe('ClaimInfoDetailsComponent', () => {
-    beforeEach(waitForAsync(() => {
-        TestBed.configureTestingModule({
-            declarations: [ClaimInfoDetailsComponent],
+    beforeEach(
+        waitForAsync(() => {
+            TestBed.configureTestingModule({
+                declarations: [ClaimInfoDetailsComponent],
+            })
+                .compileComponents()
+                .then();
         })
-            .compileComponents()
-            .then();
-    }));
-    it('should create component', waitForAsync(() => {
-        const fixture = TestBed.createComponent(ClaimInfoDetailsComponent);
-        const component = fixture.debugElement.componentInstance;
-        expect(component).toBeTruthy();
-    }));
-    it('should render reason', waitForAsync(() => {
-        const fixture = TestBed.createComponent(ClaimInfoDetailsComponent);
-        const rendered = fixture.debugElement.componentInstance;
-        expect(rendered).toBeTruthy();
-    }));
+    );
+    it(
+        'should create component',
+        waitForAsync(() => {
+            const fixture = TestBed.createComponent(ClaimInfoDetailsComponent);
+            const component = fixture.debugElement.componentInstance;
+            expect(component).toBeTruthy();
+        })
+    );
+    it(
+        'should render reason',
+        waitForAsync(() => {
+            const fixture = TestBed.createComponent(ClaimInfoDetailsComponent);
+            const rendered = fixture.debugElement.componentInstance;
+            expect(rendered).toBeTruthy();
+        })
+    );
 });
