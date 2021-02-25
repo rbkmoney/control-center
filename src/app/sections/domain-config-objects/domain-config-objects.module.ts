3d909a39f438c3e46fc90d3fa2f49ecb72ebc101
Ildar Galeev <keinasylum@gmail.com>
2021-02-25 18:12:51 +0300

    Add mat selection list sample

--- src/app/sections/domain-config-objects/domain-config-objects.module.ts
+++ src/app/sections/domain-config-objects/domain-config-objects.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 import { CollapseModule } from '@cc/components/collapse';
 
@@ -8,7 +10,14 @@ import { DomainConfigRoutingModule } from './domain-config-objects-routing.modul
 import { DomainConfigObjectsComponent } from './domain-config-objects.component';
 
 @NgModule({
-    imports: [CommonModule, DomainConfigRoutingModule, FlexModule, CollapseModule],
+    imports: [
+        CommonModule,
+        DomainConfigRoutingModule,
+        FlexModule,
+        CollapseModule,
+        MatCardModule,
+        MatListModule,
+    ],
     declarations: [DomainConfigObjectsComponent],
 })
 export class DomainConfigObjectsModule {}
