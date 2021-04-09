3319fd8fa2aeb146560b41b26c007ad151e2762a
Ildar Galeev <keinasylum@gmail.com>
2021-04-09 17:39:31 +0300

    Bump damsel version

--- src/app/sections/payment-details/payment-main-info/payment-tool/bank-card/bank-card.component.ts
+++ src/app/sections/payment-details/payment-main-info/payment-tool/bank-card/bank-card.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 import {
-    BankCardPaymentSystem,
-    BankCardTokenProvider,
+    LegacyBankCardPaymentSystem,
+    LegacyBankCardTokenProvider,
 } from '../../../../../thrift-services/damsel/gen-model/domain';
 import { BankCard } from '../../../../../thrift-services/damsel/gen-model/merch_stat';
 
@@ -14,6 +14,7 @@ import { BankCard } from '../../../../../thrift-services/damsel/gen-model/merch_
 export class BankCardComponent {
     @Input() bankCard: BankCard;
 
-    paymentSystems = BankCardPaymentSystem;
-    tokenProviders = BankCardTokenProvider;
+    // TODO Need migration according to: https://github.com/rbkmoney/damsel/commit/61677b86006d405619bdc5f23d6416a929688180
+    legacyPaymentSystems = LegacyBankCardPaymentSystem;
+    legacyTokenProviders = LegacyBankCardTokenProvider;
 }
