2fdef248dac4d1e514fe95c41a97f6f9a0154b28
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-09 17:31:35 +0300

    simplify call

--- src/app/api/fistful/wallet/management.service.ts
+++ src/app/api/fistful/wallet/management.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 
 import { KeycloakTokenInfoService } from '@cc/app/shared/services';
 
 import { ThriftConnector } from '../../thrift-connector';
-import { WalletState } from '../gen-model/wallet';
+import { WalletState, EventRange as EventRangeModel } from '../gen-model/wallet';
 import * as Management from './gen-nodejs/Management';
 import { EventRange } from './gen-nodejs/base_types';
 
@@ -15,9 +14,7 @@ export class ManagementService extends ThriftConnector {
         super(keycloakTokenInfoService, Management, '/v1/wallet');
     }
 
-    get(walletID: string, range = new EventRange()): Observable<WalletState> {
-        return this.prepareThriftServiceMethod<WalletState>('Get').pipe(
-            switchMap((fn) => fn(walletID, range))
-        );
+    get(walletID: string, range: EventRangeModel = new EventRange()): Observable<WalletState> {
+        return this.callThriftServiceMethod('Get', walletID, range);
     }
 }
