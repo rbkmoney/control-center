ecf3a7cd2ed05891dba188ba0f3583b0f0439cb8
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-27 14:50:03 +0300

    1

--- src/app/monaco-editor/model.ts
+++ src/app/monaco-editor/model.ts
@@ -1,16 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export type IEditorOptions = monaco.editor.IEditorOptions;
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export type IDiffEditorOptions = monaco.editor.IDiffEditorOptions;
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export type ITextModel = monaco.editor.ITextModel;
 export type CancellationToken = monaco.CancellationToken;
 export type ProviderResult<T> = monaco.languages.ProviderResult<T>;
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export type IDisposable = monaco.IDisposable;
 export type Position = monaco.Position;
 export type CompletionList = monaco.languages.CompletionList;
 export type CompletionContext = monaco.languages.CompletionContext;
 export type CodeLensList = monaco.languages.CodeLensList;
 export type CodeLens = monaco.languages.CodeLens;
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export type IEditor = monaco.editor.IEditor;
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export type IStandaloneCodeEditor = monaco.editor.IStandaloneCodeEditor;
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export type IStandaloneDiffEditor = monaco.editor.IStandaloneDiffEditor;
 
 export interface MonacoFile {
