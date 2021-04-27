2f1e3c22b1a3c00dfa138095d65fc9df05ab1775
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-27 15:59:40 +0300

    ContractorId

--- src/app/shared/components/party-modifications-extractor/extract-form-value.ts
+++ src/app/shared/components/party-modifications-extractor/extract-form-value.ts
@@ -3,12 +3,12 @@ import {
     PaymentInstitutionRef,
 } from '../../../thrift-services/damsel/gen-model/domain';
 
-interface ContractorID {
+interface ContractorId {
     id: string;
 }
 
 export interface ExtractFormValue {
     category: CategoryRef;
     payment_institution: PaymentInstitutionRef;
-    contractor: ContractorID;
+    contractor: ContractorId;
 }
