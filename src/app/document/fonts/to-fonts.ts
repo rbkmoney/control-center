b2c414fbc43038ea779e9eaa1c5a8ec37d5768ad
hedgehog <ezhoden@mail.ru>
2021-06-16 17:55:31 +0300

    Revert to-fonts

--- src/app/document/fonts/to-fonts.ts
+++ src/app/document/fonts/to-fonts.ts
@@ -3,7 +3,7 @@ import { TFontFamilyTypes } from 'pdfmake/build/pdfmake';
 import { Font } from './font';
 
 function familyToFonts(familyName: string | number, family: TFontFamilyTypes): Font[] {
-    return Object.entries(family).map(([type, url]) => ({
+    return (Object.entries(family) as [string, string][]).map(([type, url]) => ({
         family: familyName,
         type,
         hash: `${familyName}_${type}`,
