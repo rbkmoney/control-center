a3844bae82a90c116eabaaf26d8b0d6c2322201b
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-09-21 15:59:29 +0300

    fix layout

--- src/app/shared/components/party-modification-forms/contract/payout-tool-info/payout-tool-info.component.ts
+++ src/app/shared/components/party-modification-forms/contract/payout-tool-info/payout-tool-info.component.ts
@@ -8,7 +8,6 @@ enum Type {
     RussianBankAccount = 'russian_bank_account',
     InternationalBankAccount = 'international_bank_account',
     WalletInfo = 'wallet_info',
-    PaymentInstitutionAccount = 'payment_institution_account',
 }
 
 @Component({
@@ -24,26 +23,16 @@ export class PayoutToolInfoComponent implements OnInit {
 
     selected: Type;
 
-    types = [
-        Type.RussianBankAccount,
-        Type.InternationalBankAccount,
-        Type.WalletInfo,
-        Type.PaymentInstitutionAccount,
-    ];
+    types = [Type.RussianBankAccount, Type.InternationalBankAccount, Type.WalletInfo];
 
     t = Type;
 
     constructor(private fb: FormBuilder) {}
 
-    ngOnInit(): void {
+    ngOnInit() {
         const russianBankAccount = get(this, 'initialValue.russian_bank_account', null);
         const internationalBankAccount = get(this, 'initialValue.international_bank_account', null);
         const walletInfo = get(this, 'initialValue.wallet_info', null);
-        const paymentInstitutionAccount = get(
-            this,
-            `initialValue.${Type.PaymentInstitutionAccount}`,
-            null
-        );
         if (russianBankAccount) {
             this.selected = Type.RussianBankAccount;
         }
@@ -53,31 +42,26 @@ export class PayoutToolInfoComponent implements OnInit {
         if (walletInfo) {
             this.selected = Type.WalletInfo;
         }
-        if (paymentInstitutionAccount) {
-            this.selected = Type.PaymentInstitutionAccount;
-        }
         this.select();
         this.form.updateValueAndValidity();
     }
 
-    select(): void {
-        this.clearControl();
+    select() {
         switch (this.selected) {
             case Type.RussianBankAccount:
+                this.clearControl();
                 this.form.registerControl(Type.RussianBankAccount, this.fb.group({}));
                 break;
             case Type.InternationalBankAccount:
+                this.clearControl();
                 this.form.registerControl(Type.InternationalBankAccount, this.fb.group({}));
                 break;
             case Type.WalletInfo: {
+                this.clearControl();
                 const walletInfo = get(this, 'initialValue.wallet_info', {});
                 this.form.registerControl(Type.WalletInfo, this.fb.group(walletInfo));
                 break;
             }
-            case Type.PaymentInstitutionAccount:
-                this.form.registerControl(Type.PaymentInstitutionAccount, this.fb.control({}));
-                this.form.patchValue({});
-                break;
         }
     }
 
