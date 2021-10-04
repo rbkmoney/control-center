28ca4b88c517c87a6e119168da5a5e1b30ba29d6
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-10-04 21:03:43 +0300

    add debounce

--- src/app/shared/components/merchant-field/merchant-field.component.ts
+++ src/app/shared/components/merchant-field/merchant-field.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component, Injector, Input, OnInit } from '@angular/core';
 import { FormControl } from '@ngneat/reactive-forms';
-import { UntilDestroy } from '@ngneat/until-destroy';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { FormControlSuperclass, provideValueAccessor } from '@s-libs/ng-core';
 import { coerceBoolean } from 'coerce-property';
-import { of, ReplaySubject, Subject } from 'rxjs';
-import { map, startWith, switchMap, tap, withLatestFrom } from 'rxjs/operators';
+import { Observable, of, ReplaySubject, Subject } from 'rxjs';
+import { debounceTime, map, startWith, switchMap, tap, withLatestFrom } from 'rxjs/operators';
 
 import { Option } from '@cc/components/select-search-field';
 
@@ -25,6 +25,7 @@ export class MerchantFieldComponent extends FormControlSuperclass<Party> impleme
     formControl = new FormControl<Party>();
     incomingValue$ = new Subject<Partial<Party>>();
     options$ = new ReplaySubject<Option<Party>[]>(1);
+    searchChange$ = new Subject<string>();
 
     constructor(injector: Injector, private deanonimusService: DeanonimusService) {
         super(injector);
@@ -38,28 +39,36 @@ export class MerchantFieldComponent extends FormControlSuperclass<Party> impleme
                     if (!value?.id) return of<Party>(null);
                     const v = options.find((o) => o.value.id === value.id);
                     if (v) return of(v.value);
-                    return this.deanonimusService.searchParty(value.id).pipe(
-                        map((parties) =>
-                            parties.map((p) => ({ label: p.party.email, value: p.party }))
-                        ),
+                    return this.searchOptions(value?.id).pipe(
                         tap((options) => this.options$.next(options)),
-                        map((options) => options.find((o) => o.value.id === value.id).value || null)
+                        map(
+                            (options) =>
+                                options?.find((o) => o.value.id === this.formControl.value?.id)
+                                    ?.value || null
+                        )
                     );
-                })
+                }),
+                untilDestroyed(this)
             )
             .subscribe((v) => this.formControl.setValue(v));
         this.formControl.valueChanges.subscribe((v) => this.emitOutgoingValue(v));
-    }
-
-    searchChange(str: string): void {
-        this.deanonimusService
-            .searchParty(str)
-            .pipe(map((parties) => parties.map((p) => ({ label: p.party.email, value: p.party }))))
+        this.searchChange$
+            .pipe(
+                debounceTime(600),
+                switchMap((str) => this.searchOptions(str)),
+                untilDestroyed(this)
+            )
             .subscribe((options) => this.options$.next(options));
     }
 
     handleIncomingValue(partyLike: Partial<Party>): void {
         this.formControl.setValue(partyLike as Party);
         this.incomingValue$.next(partyLike);
+    }
+
+    private searchOptions(str: string): Observable<Option<Party>[]> {
+        return this.deanonimusService
+            .searchParty(str)
+            .pipe(map((parties) => parties.map((p) => ({ label: p.party.email, value: p.party }))));
     }
 }
